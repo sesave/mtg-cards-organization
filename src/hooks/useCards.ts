08c5f8342c30f21fe363d6a1d479b8741c6e09a8
sesave <sebastiansanchezvega@gmail.com>
2023-08-27 14:07:41 -0300

    feat: set the result on a state

--- src/hooks/useCards.ts
+++ src/hooks/useCards.ts
@@ -3,9 +3,11 @@ import { CardsContext } from "../contexts/CardsContext";
 import * as Scry from "scryfall-sdk";
 
 const useCards = () => {
+  const { setCardsResults } = useContext<any>(CardsContext);
   const getCardsInfo = async (debouncedSearch: string) => {
-    const chalice = await Scry.Cards.byName(debouncedSearch);
-    console.log(chalice.name, chalice.set);
+    const results = await Scry.Cards.byName(debouncedSearch, true);
+    console.log(results);
+    setCardsResults(results);
   };
 
   return { getCardsInfo };
